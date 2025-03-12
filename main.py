d5e3880119a7a405263e929a16ae9d6c33ae50cd
MoonMadScholar <Uchihagod0519@gmail.com>
2025-03-12 04:27:52 -0600

    B: add comments and reformated the code

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from math import log
-
+# A to the power of x equals' B
+# solve for x
 def main():
     print(solve_for_ex(4,1024))
 
 def solve_for_ex(a,b):
-    """ex = 0
+  # orginal method to solve problem
+  """
+    ex = 0
     for i in range(0,b):
         if b == a ** i:
             return i
     return ex
-"""
-    return round(log(b,a))
+  """
+  return round(log(b,a))
+    
 main()
